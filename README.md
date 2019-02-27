f7cb9f77d07e3dca223979b12918bbc840e9bba7
JO <5667502+folde01@users.noreply.github.com>
2019-02-27 16:17:16 +0000

    Add mongodb (running on localhost), mongoose (with a model for tutorials), show.ejs template to view a specific tutorial, createStarterData() to populate db with dummy tutorials.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TechGrazer
 
 This is a portfolio project, aimed at helping me get my first job as a web developer. The code is for a tech tutorial web site called techgrazer.com. I'm building it to solidify my understanding of the technologies I learned while completing Colt Steele's excellent Web Developer Bootcamp on Udemy. Those technologies include HTML, CSS, Bootstrap, Javascript, jQuery, Node.js, Express.js, MongoDB, Mongoose.js, Passport.js and Heroku. The bootcamp was my first real taste of web development, so much of the code for TechGrazer is adapted from the bootcamp's code-alongs.
+
+The tutorials? I'm hoping to write them along the way, in part based on my own learning from building the site, and perhaps partly on past experience as a Linux admin.
+
+Why MongoDB? First and foremost, admittedly, I want to use it again after the bootcamp so it sticks. It also arguably makes sense in the context, which is a CMS for tutorials that don't exist yet, running on a site that built by someone who isn't a web developer yet, meaning it will start out with very basic features and become more sophisticated as I learn. The schema for such a project seems likely to change quite a bit, making a NoSQL database like Mongo a decent option - see e.g. https://medium.com/xplenty-blog/the-sql-vs-nosql-difference-mysql-vs-mongodb-32c9980e67b2
--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 'use strict';
 /*jshint esversion: 6 */
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var tutorials = [
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+const mongo_url = 'mongodb://localhost:27017/techgrazer';
+mongoose.connect(mongo_url, {useNewUrlParser: true});
+
+app.set('view engine', 'ejs');
+app.use(express.static('images'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var tutorialSchema = new mongoose.Schema({
+    title: String,
+    date: String,
+    image: String,
+    description: String,
+    text: String
+});
+
+var Tutorial = mongoose.model('Tutorial', tutorialSchema);
+
+const starterData = [
     {
         title: 'Tutorial 1 title',
         date: '25 Feb 2019',
         image: 'laptop-on-desk.jpg',
+        description: `Tutorial 1 description. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod.`,
         text: `Tutorial 1 body. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod eloquentiam, eos ex causae indoctum. In ius aperiri eripuit definitiones, nec in posse urbanitas. Vim malis oratio elaboraret ad. Mei accumsan adipisci periculis ut, per apeirian temporibus an. Ne usu invidunt praesent.
 
         Usu et vocibus repudiare. Modo impetus est id, luptatum posidonium eu pri. Ad vim quot aeque, eos et partem apeirian. Eum ne veritus legendos constituam, no inciderint reformidans ius. Eos verterem gubergren et, postulant voluptaria sit cu, ad voluptua electram gloriatur qui. Movet solet eu usu, eum ex choro ignota eloquentiam, ius novum nostrum necessitatibus ea.
@@ -23,6 +43,7 @@ var tutorials = [
         title: 'Tutorial 2 title',
         date: '26 Feb 2019',
         image: 'programmers-in-an-office.jpg',
+        description: `Tutorial 2 description. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod.`,
         text: `Tutorial 2 body. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod eloquentiam, eos ex causae indoctum. In ius aperiri eripuit definitiones, nec in posse urbanitas. Vim malis oratio elaboraret ad. Mei accumsan adipisci periculis ut, per apeirian temporibus an. Ne usu invidunt praesent.
 
         Usu et vocibus repudiare. Modo impetus est id, luptatum posidonium eu pri. Ad vim quot aeque, eos et partem apeirian. Eum ne veritus legendos constituam, no inciderint reformidans ius. Eos verterem gubergren et, postulant voluptaria sit cu, ad voluptua electram gloriatur qui. Movet solet eu usu, eum ex choro ignota eloquentiam, ius novum nostrum necessitatibus ea.
@@ -37,6 +58,7 @@ var tutorials = [
         title: 'Tutorial 3 title',
         date: '27 Feb 2019',
         image: 'pair-programmers.jpg',
+        description: `Tutorial 3 description. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod.`,
         text: `Tutorial 3 body. Lorem ipsum dolor sit amet, sed tale accusamus at, vel no euismod eloquentiam, eos ex causae indoctum. In ius aperiri eripuit definitiones, nec in posse urbanitas. Vim malis oratio elaboraret ad. Mei accumsan adipisci periculis ut, per apeirian temporibus an. Ne usu invidunt praesent.
 
         Usu et vocibus repudiare. Modo impetus est id, luptatum posidonium eu pri. Ad vim quot aeque, eos et partem apeirian. Eum ne veritus legendos constituam, no inciderint reformidans ius. Eos verterem gubergren et, postulant voluptaria sit cu, ad voluptua electram gloriatur qui. Movet solet eu usu, eum ex choro ignota eloquentiam, ius novum nostrum necessitatibus ea.
@@ -49,37 +71,72 @@ var tutorials = [
     }
 ];
 
-app.set('view engine', 'ejs');
-app.use(express.static('images'));
-app.use(bodyParser.urlencoded({ extended: true }));
+function createStarterData(){
+    console.log('Creating starter data.');
+    starterData.forEach(function(tutorial){
+        Tutorial.create({
+           title: tutorial.title,
+           date: tutorial.date,
+           image: tutorial.image,
+           description: tutorial.description,
+           text: tutorial.text
+        }, function(err, tutorial){
+            if (err) {
+                console.log(err);
+            }
+        });
+    });
+}
 
 app.get('/', function(req, res) {
     res.redirect('/tutorials');
 });
 
 app.get('/tutorials', function(req, res) {
-    res.render('tutorials', { tutorials: tutorials });
+    // get all tutorials from db
+    Tutorial.find({}, function(err, allTutorials){
+        if (err){
+            console.log(err);
+        }
+        // ok to render even if err?
+        res.render('index', {tutorials: allTutorials});
+    });
 });
 
 app.get('/tutorials/new', function(req, res) {
     res.render('new');
-
 });
+
 app.post('/tutorials', function(req, res) {
     // get data from form and add to tutorials array
     const newTutorial = {
         title: req.body.title,
         date: req.body.date,
         image: req.body.image,
+        description: tutorial.description,
         text: req.body.text,
     };
-    tutorials.push(newTutorial);
-
-    // redirect to tutorials page
-    // note: res.redirect defaults to GET route
-    res.redirect('/tutorials');
-
+    // tutorials.push(newTutorial);
+    Tutorial.create(newTutorial, function(err, tutorial){
+        if (err){
+            console.log(err);
+        } 
+        res.redirect('/tutorials');
+    });
 });
+
+app.get('/tutorials/:id', function(req, res){
+    Tutorial.findById(req.params.id, function(err, foundTutorial){
+        if (err){
+            console.log(err);
+        }
+        res.render('show', {tutorial: foundTutorial});
+    });
+});
+
+if (process.argv[2] === 'createStarterData'){
+    createStarterData();
+} 
 
 app.listen(4444, 'localhost', function() {
     console.log('techgrazer is running!!!!');
